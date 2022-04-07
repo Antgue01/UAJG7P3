93e545dd6c2719025494b75d988766799d83762a
Antgue01 <antgue01@ucm.es>
2022-04-07 16:32:05 +0200

    serializer

---
+++ Indie/Assets/Telemetry/JSONSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UAJ
+{
+
+    public class JSONSerializer : ISerializer
+    {
+        public string Serialize(TrackerEvent e)
+        {
+            return e.toJSON();
+        }
+    }
+}
